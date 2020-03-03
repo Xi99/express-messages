1f700b585cf02bdbebcb911ff26defea6d16723f
Sean Lorman <sean.lorman@gmail.com>
2020-03-03 14:33:08 -0700

    GET route set up for all messages

--- db/pgconfig.js
+++ db/pgconfig.js
@@ -3,12 +3,22 @@ const pool = new Pool({
   database: "messages"
 });
 
-pool.getAllMessages = () => {
-  database.query("SELECT * FROM messages").then(res => console.log(res));
+const sendMessage = message => {
+  let values = [message_name, message_text];
+  return pool
+    .query("INSERT INTO messages VALUES ($1, $2)", values)
+    .then(res => console.log("message: ", res.rows));
 };
 
-module.exports = pool;
+const getAllMessages = () => {
+  return pool.query("SELECT * FROM messages").then(res => res.rows);
+};
 
+module.exports = {
+  pool,
+  getAllMessages,
+  sendMessage
+};
 // CREATE TABLE messages (
 //   message_id SERIAL,
 //   message_name TEXT,
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const app = express();
 const pool = require("./db/pgconfig");
+const bp = require("body-parser");
+// const cors = require("cors");
 
-app.use((req, res, next) => {
-  res.status(404).send("That route does not exist");
+const app = express();
+app.use(bp.json());
+// app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.status(404).send("That route does not exist");
+// });
+
+app.get("/api/messages", (req, res) => {
+  pool.getAllMessages().then(results => res.send(results));
 });
 
-app.get("/api/messages", (res, req) => {
-  pool.gelAllMessages().then(results => res.send(results));
-});
+// app.post("/api/messages", (res, req) => {
+//   pool.sendMessage().then(result => res.send(results))
+// })
 
 const port = 3000;
 
